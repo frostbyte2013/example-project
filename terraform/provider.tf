b100ec95fa87f08152d5e2b8c9f9478340c303fd
frostbyte2013 <nickandrew.lyp@gmail.com>
2024-04-05 16:46:36 -0400

    set-tf-provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,13 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "5.42.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.44.0"
+    }
+  }
+}
 
-# provider "aws" {
-#   # Configuration options
-#   region = "us-east-1"
-# }
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
